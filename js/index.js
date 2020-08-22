15aba6d2c6d91137aaf5e052b6ce97145922d0b9
diep <diep.le@0004s.com>
2020-08-22 13:33:23 +0900

    create func

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
+function checkNgto(x) {
+	var flag = true;
+	if (x < 2) flag = false;
+	else {
+		for (var i = 2; i <= Math.sqrt(x); i++) {
+			if (x % i == 0) {
+				flag = false;
+				break;
+			}
+		}
+	}
+	return flag;
+}
 
-
+function check() {
+	var input = document.getElementById("nguyento");
+	checkNgto(input.value)
+		? alert(`${input.value} la so nguyen to`)
+		: alert("khong phai so nguyen to");
+}
